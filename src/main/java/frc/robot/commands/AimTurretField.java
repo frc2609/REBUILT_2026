bc1b3c4381dc1b723d160b6a11f5250a2c47b2ac
billy <billy.parmenter98@gmail.com>
2026-03-17 19:56:46 -0400

    updated fudge on targetting

--- src/main/java/frc/robot/commands/AimTurretField.java
+++ src/main/java/frc/robot/commands/AimTurretField.java
@@ -88,7 +88,7 @@ public class AimTurretField extends Command {
         turret.setHoodPosition();
         flywheel.setAutoSpeed(power.get()*shot.rpm()/60.0);
         double turretAngleDeg = shot.launcherAngle()
-            .minus(new Rotation2d().fromDegrees(-145.0))
+            .minus(new Rotation2d().fromDegrees(-170.0))
             .minus(swerve.getRotation())
             .getDegrees();
 
